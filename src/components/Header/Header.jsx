377142c622367dde2f52074ab2b6dfc77616aec2
unknown <112sayedalsahab@gmail.com>
2026-03-11 21:33:45 +0600

    add nav link and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
             <h3>This is header</h3>
+            <nav>
+              
+              <Link to="/">Home</Link>
+              <Link to="mobiles">Mobiles</Link>
+              <Link to="/laptops">Laptops</Link>
+              <Link to="/">Home</Link>
+            </nav>
         </div>
     );
 };
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../Header/Header';
 import { Outlet } from 'react-router';
+import Footer from '../Footer/Footer';
 
 const Root = () => {
     return (
         <div>
             <Header></Header>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
